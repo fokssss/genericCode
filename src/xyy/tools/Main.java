0c989b56101113a89018bd3bcf82bfa037422be5
xiongyy <focus315@163.com>
2017-04-02 11:24:52 +0800

    ss

--- src/xyy/tools/Main.java
+++ src/xyy/tools/Main.java
@@ -5,10 +5,12 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) {
-
         String dbUrl = "jdbc:mysql://localhost:3306/chnbs";
-        String outPath = "/Users/xyy/workspace/chnsball/src/";
+//        String outPath = "/Users/xyy/workspace/chnsball/src/";
+        String outPath = ""
+
         GenEntityMysql generic = new GenEntityMysql(dbUrl, outPath);
+
         try {
             generic.build();
         } catch (SQLException e) {
